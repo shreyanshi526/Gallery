199c8c9cb0ad0ef05bf6c444e219131d708ec0b7
shreyanshi526 <shreyanshikushwaha526@gmail.com>
2023-12-06 17:03:29 +0530

    second commit

--- src/Gallery.jsx
+++ src/Gallery.jsx
@@ -1,25 +1,54 @@
-import React from 'react'
-import img from './assets/img.png'
-const arr = [
-    {
-      "img" : img
-    },
-    {
-      "img" : img
-    },
-    {
-      "img" : img
-    }
-  ]
+import React, { useEffect, useState } from 'react'
+import img from './assets/img.png';
+import { Flex } from 'antd'
+import { Card } from 'antd';
 
- const Gallery = () => {
-    return (
-        <>
-            {arr.map((item) => (
-                <img src={item.img} />
-            ))}
-        </>
-    )
+const { Meta } = Card;
+
+function HandleClick () {
+    console.log("clicked")
+}
+
+function Image() {
+  const [data, setData] = useState();
+
+  React.useEffect(() => {
+    try {
+      fetch("https://api.unsplash.com/photos?client_id=dp2iEgOm5BfUzfPsH3Ue-nR_IZq1JSgzp5E2p4-nxBM").then((response) => {
+        response.json().then((res) => {
+          console.log(res);
+          setData(res);
+        })
+      })
+    }
+    catch (error) {
+      console.log(error)
+    }
+  }, [])
+  return data;
+}
+const Gallery = () => {
+
+  const Images = Image();
+  return (
+    <>
+
+      {Images ?
+        (Images.map((item) => {
+          return <img
+            key={item.id}
+            src={item.urls.small}
+            style={{
+              width:"19vmax",
+              height:"20vmax",
+              padding:"2vmax",
+              alignItems:"center"
+            }}
+          />
+        })) : (<p>Loading....</p>)
+      }
+    </>
+  )
 }
 
 export default Gallery
